ba1b4f2d77e009bb81f5ce74adcd5cf7018a2247
Yuno Lee <matheussanpinheiro@gmail.com>
2020-06-05 03:02:07 -0300

    Final Commit

--- MatchSumGoogle/Starter.cs
+++ MatchSumGoogle/Starter.cs
@@ -1,66 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MatchSumGoogle
 {
-    public class Starter
-    {
-        public Starter()
-        {
-            Start(10);
-            NaoOrdenada();
-        }
+	public class Starter
+	{
+		public Starter()
+		{
+			// Verifica se a soma de nois numeros no array sao X
+			Start(10);
+		}
 
-        private int[] GerarListaNaoOrdenada()
-        {
-            int positions = 5;
-            int[] vetor = new int[positions];
+		private int[] GerarListaNaoOrdenada()
+		{
+			int positions = 5;
+			int[] vetor = new int[positions];
 
-            Random r = new Random();
-            for (int i = 0; i < positions; i++)
-            {
-                vetor[i] = r.Next(1, 10);
-            }
-            return vetor;
-        }
+			Random r = new Random();
+			for (int i = 0; i < positions; i++)
+			{
+				vetor[i] = r.Next(1, 10);
+			}
+			return vetor;
+		}
 
-        private bool Start(int sum)
-        {
-            var vetor = GerarListaNaoOrdenada();
+		private void Start(int sum)
+		{
+			var vetor = GerarListaNaoOrdenada();
 
-            Array.Sort(vetor);
-            foreach (var value in vetor)
-            {
-                Console.WriteLine(value);
-            }
+			Array.Sort(vetor); // Teste com array ordenado
+			foreach (var value in vetor)
+			{
+				Console.WriteLine(value);
+			}
 
-            int MAX = vetor.Length - 1;
-            int MIN = 0;
-            for (int i = 0; i < vetor.Length; i++)
-            {
-                Console.WriteLine(vetor[MIN].ToString() + " + " + vetor[MAX].ToString() + " = " + (vetor[MIN] + vetor[MAX]));
-                Console.WriteLine();
-                if ((vetor[MIN] + vetor[MAX]) > sum)
-                {
-                    MAX -= 1;
-                    continue;
-                }
-                else if ((vetor[MIN] + vetor[MAX]) < sum)
-                {
-                    MIN += 1;
-                    continue;
-                }
-                return true;
-            }
-            return false;
+			int MAX = vetor.Length - 1;
+			int MIN = 0;
 
-        }
+			for (int i = 0; i < vetor.Length; i++)
+			{
+				Console.WriteLine(vetor[MIN].ToString() + " + " + vetor[MAX].ToString() + " = " + (vetor[MIN] + vetor[MAX]));
+				Console.WriteLine();
 
-        private bool NaoOrdenada()
-        {
-            var vetor = GerarListaNaoOrdenada();
-            return false;
-        }
-    }
+				if ((vetor[MIN] + vetor[MAX]) > sum)
+				{
+					MAX -= 1;
+				}
+				else if ((vetor[MIN] + vetor[MAX]) < sum)
+				{
+					MIN += 1;
+				}
+
+
+			}
+		}
+	}
 }
